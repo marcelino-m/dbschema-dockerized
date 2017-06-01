f4f5b4031fcd1e5cef6f209dde8252249902c67a
Marcelo M. A <ma.munoz.araya@gmail.com>
2017-06-01 03:24:47 -0400

    Fix a stupid error!

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ DOCKERUSER=${1:-marcelino}
 
 wget http://www.dbschema.com/download/DbSchema_unix_7_5_1.tar.gz -O dbschema.tar.gz
 tar xvf dbschema.tar.gz
-docker build -t "$DOCKERUSER"/dbschema:v7.5.1 -t "$DOCKERUSER"/dbschema:latest -t .
+docker build -t "$DOCKERUSER"/dbschema:v7.5.1 -t "$DOCKERUSER"/dbschema:latest .
